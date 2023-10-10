a7a4148882be7441bd5ed5e13bb8c73f445aaabd
Payam Zahedi <pzahedi1995@gmail.com>
2023-10-10 13:05:20 +0330

    init commit

---
+++ lib/src/home_feature/home_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class HomeView extends StatelessWidget {
+
+  static const routeName = '/';
+
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/src/home_feature/home_widgets.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class HomeContainer extends StatelessWidget {
+  const HomeContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
