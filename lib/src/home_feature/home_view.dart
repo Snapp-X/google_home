fe9d0bc6454db39672a4c90149ed3fc7da41725e
Payam Zahedi <pzahedi1995@gmail.com>
2023-10-10 13:20:44 +0330

    add home container widget

--- lib/src/home_feature/home_view.dart
+++ lib/src/home_feature/home_view.dart
@@ -1,13 +1,35 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:google_home/src/home_feature/home_widgets.dart';
 
+// TODO(payam): add theme and colors to the app
 class HomeView extends StatelessWidget {
-
   static const routeName = '/';
 
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: Color(0xFF18181E),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            HomeContainer(
+              width: 100,
+              height: 100,
+              colorMode: ColorMode.gradient,
+            ),
+            SizedBox(height: 20),
+            HomeContainer(
+              width: 100,
+              height: 100,
+              colorMode: ColorMode.solid,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/src/home_feature/home_widgets.dart
+++ lib/src/home_feature/home_widgets.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/widgets.dart';
 
+enum ColorMode { gradient, solid }
+
 class HomeContainer extends StatelessWidget {
-  const HomeContainer({super.key});
+  const HomeContainer({
+    super.key,
+    this.width,
+    this.height,
+    this.borderRadius = 15,
+    this.colorMode = ColorMode.solid,
+    this.padding = EdgeInsets.zero,
+    this.margin = EdgeInsets.zero,
+    this.child,
+  });
+
+  final double? width;
+  final double? height;
+  final double borderRadius;
+  final ColorMode colorMode;
+  final EdgeInsetsGeometry padding;
+  final EdgeInsetsGeometry margin;
+
+  final Widget? child;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: width,
+      height: height,
+      padding: padding,
+      margin: margin,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(borderRadius),
+        gradient: colorMode == ColorMode.gradient
+            ? const LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color(0xff18181e), Color(0xff2e2e3d)],
+              )
+            : null,
+        color: colorMode == ColorMode.solid ? const Color(0xFF1e1e26) : null,
+      ),
+      child: child,
+    );
   }
 }
