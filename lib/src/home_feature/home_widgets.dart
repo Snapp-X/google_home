5e2cd6bc96645d398e8f9519e15590af16ecaf38
Payam Zahedi <pzahedi1995@gmail.com>
2023-10-11 14:05:19 +0330

    update light card

--- lib/src/home_feature/home_widgets.dart
+++ lib/src/home_feature/home_widgets.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'dart:math' as math;
 
 enum ColorMode { gradient, solid }
 
@@ -118,19 +119,327 @@ class LightCard extends StatelessWidget {
     final theme = Theme.of(context);
     return HomeContainer(
       borderRadius: 20,
-      child: Align(
-        alignment: Alignment.topCenter,
-        child: Padding(
-          padding: const EdgeInsets.only(top: 16.0),
-          child: Text(
-            'LIGHT',
-            style: theme.textTheme.labelSmall?.copyWith(
-              height: 1.2,
-              fontSize: 8,
-              fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.onSurface,
+      child: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Image.asset('assets/images/lamp.png'),
+                ),
+                // TODO(payam): add animation for light
+              ],
             ),
           ),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'LIGHT',
+                        style: theme.textTheme.labelSmall?.copyWith(
+                          height: 1.2,
+                          fontSize: 8,
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                      ),
+                      Transform.scale(
+                        scale: .6,
+                        child: Switch(
+                          value: true,
+                          onChanged: (value) {},
+                          activeColor: const Color(0xFF381E72),
+                          activeTrackColor: const Color(0xFF5771C1),
+                          inactiveThumbColor: const Color(0xFF18181E),
+                          inactiveTrackColor: const Color(0xFF2F2F3E),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Spacer(flex: 2),
+
+                  /// Row with 4 equal square container
+                  const ColorPicker(),
+                  const Spacer(),
+                  SliderTheme(
+                    data: const SliderThemeData(
+                      trackHeight: 14,
+                      inactiveTrackColor: Color(0xff2F2F3E),
+                      activeTrackColor: Color(0xff5771C1),
+                      trackShape: RoundedDeepSliderTrackShape(),
+                      thumbColor: Color(0xff393760),
+                      thumbShape: RoundSliderThumbShape(
+                        enabledThumbRadius: 6,
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                      child: Slider(
+                        value: .5,
+                        onChanged: (newValue) {},
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RoundedDeepSliderTrackShape extends SliderTrackShape {
+  const RoundedDeepSliderTrackShape();
+
+  @override
+  void paint(
+    PaintingContext context,
+    Offset offset, {
+    required RenderBox parentBox,
+    required SliderThemeData sliderTheme,
+    required Animation<double> enableAnimation,
+    required TextDirection textDirection,
+    required Offset thumbCenter,
+    Offset? secondaryOffset,
+    bool isDiscrete = false,
+    bool isEnabled = false,
+    double additionalActiveTrackHeight = 2,
+  }) {
+    assert(sliderTheme.disabledActiveTrackColor != null);
+    assert(sliderTheme.disabledInactiveTrackColor != null);
+    assert(sliderTheme.activeTrackColor != null);
+    assert(sliderTheme.inactiveTrackColor != null);
+    assert(sliderTheme.thumbShape != null);
+    // If the slider [SliderThemeData.trackHeight] is less than or equal to 0,
+    // then it makes no difference whether the track is painted or not,
+    // therefore the painting can be a no-op.
+    if (sliderTheme.trackHeight == null || sliderTheme.trackHeight! <= 0) {
+      return;
+    }
+
+    final thumbSize = sliderTheme.thumbShape!.getPreferredSize(true, false);
+
+    // Assign the track segment paints, which are leading: active and
+    // trailing: inactive.
+    final ColorTween activeTrackColorTween = ColorTween(
+        begin: sliderTheme.disabledActiveTrackColor,
+        end: sliderTheme.activeTrackColor);
+    final ColorTween inactiveTrackColorTween = ColorTween(
+        begin: sliderTheme.disabledInactiveTrackColor,
+        end: sliderTheme.inactiveTrackColor);
+    final Paint activePaint = Paint()
+      ..color = activeTrackColorTween.evaluate(enableAnimation)!;
+    final Paint inactivePaint = Paint()
+      ..color = inactiveTrackColorTween.evaluate(enableAnimation)!;
+    final Paint leftTrackPaint;
+    final Paint rightTrackPaint;
+    switch (textDirection) {
+      case TextDirection.ltr:
+        leftTrackPaint = activePaint;
+        rightTrackPaint = inactivePaint;
+      case TextDirection.rtl:
+        leftTrackPaint = inactivePaint;
+        rightTrackPaint = activePaint;
+    }
+
+    final Rect trackRect = getPreferredRect(
+      parentBox: parentBox,
+      offset: offset,
+      sliderTheme: sliderTheme,
+      isEnabled: isEnabled,
+      isDiscrete: isDiscrete,
+    );
+    final Radius trackRadius = Radius.circular(trackRect.height / 2);
+    final Radius activeTrackRadius =
+        Radius.circular((trackRect.height + additionalActiveTrackHeight) / 2);
+
+    context.canvas.drawRRect(
+      RRect.fromLTRBAndCorners(
+        trackRect.left - (additionalActiveTrackHeight / 2),
+        trackRect.top - (additionalActiveTrackHeight / 2),
+        trackRect.right + (additionalActiveTrackHeight / 2),
+        trackRect.bottom + (additionalActiveTrackHeight / 2),
+        topRight: (textDirection == TextDirection.rtl)
+            ? activeTrackRadius
+            : trackRadius,
+        bottomRight: (textDirection == TextDirection.rtl)
+            ? activeTrackRadius
+            : trackRadius,
+        topLeft: (textDirection == TextDirection.rtl)
+            ? trackRadius
+            : activeTrackRadius,
+        bottomLeft: (textDirection == TextDirection.rtl)
+            ? trackRadius
+            : activeTrackRadius,
+      ),
+      rightTrackPaint,
+    );
+
+    context.canvas.drawRRect(
+      RRect.fromLTRBAndCorners(
+        trackRect.left,
+        trackRect.top,
+        thumbCenter.dx +
+            (thumbSize.longestSide / 2) +
+            additionalActiveTrackHeight,
+        trackRect.bottom,
+        topLeft: (textDirection == TextDirection.ltr)
+            ? activeTrackRadius
+            : trackRadius,
+        bottomLeft: (textDirection == TextDirection.ltr)
+            ? activeTrackRadius
+            : trackRadius,
+        topRight: (textDirection == TextDirection.rtl)
+            ? activeTrackRadius
+            : trackRadius,
+        bottomRight: (textDirection == TextDirection.rtl)
+            ? activeTrackRadius
+            : trackRadius,
+      ),
+      leftTrackPaint,
+    );
+
+    final bool showSecondaryTrack = (secondaryOffset != null) &&
+        ((textDirection == TextDirection.ltr)
+            ? (secondaryOffset.dx > thumbCenter.dx)
+            : (secondaryOffset.dx < thumbCenter.dx));
+
+    if (showSecondaryTrack) {
+      final ColorTween secondaryTrackColorTween = ColorTween(
+          begin: sliderTheme.disabledSecondaryActiveTrackColor,
+          end: sliderTheme.secondaryActiveTrackColor);
+      final Paint secondaryTrackPaint = Paint()
+        ..color = secondaryTrackColorTween.evaluate(enableAnimation)!;
+      if (textDirection == TextDirection.ltr) {
+        context.canvas.drawRRect(
+          RRect.fromLTRBAndCorners(
+            thumbCenter.dx,
+            trackRect.top,
+            secondaryOffset.dx,
+            trackRect.bottom,
+            topRight: trackRadius,
+            bottomRight: trackRadius,
+          ),
+          secondaryTrackPaint,
+        );
+      } else {
+        context.canvas.drawRRect(
+          RRect.fromLTRBAndCorners(
+            secondaryOffset.dx,
+            trackRect.top,
+            thumbCenter.dx,
+            trackRect.bottom,
+            topLeft: trackRadius,
+            bottomLeft: trackRadius,
+          ),
+          secondaryTrackPaint,
+        );
+      }
+    }
+  }
+
+  @override
+  Rect getPreferredRect({
+    required RenderBox parentBox,
+    Offset offset = Offset.zero,
+    required SliderThemeData sliderTheme,
+    bool isEnabled = false,
+    bool isDiscrete = false,
+  }) {
+    final double overlayWidth =
+        sliderTheme.overlayShape!.getPreferredSize(isEnabled, isDiscrete).width;
+    final double trackHeight = sliderTheme.trackHeight!;
+    assert(overlayWidth >= 0);
+    assert(trackHeight >= 0);
+
+    final double trackLeft = offset.dx;
+    final double trackTop =
+        offset.dy + (parentBox.size.height - trackHeight) / 2;
+    final double trackRight = parentBox.size.width;
+    final double trackBottom = trackTop + trackHeight;
+    // If the parentBox'size less than slider's size the trackRight will be less than trackLeft, so switch them.
+    return Rect.fromLTRB(math.min(trackLeft, trackRight), trackTop,
+        math.max(trackLeft, trackRight), trackBottom);
+  }
+}
+
+class ColorPicker extends StatelessWidget {
+  const ColorPicker({
+    super.key,
+    this.selectedColor,
+    this.onChange,
+  });
+
+  final Color? selectedColor;
+  final ValueChanged<Color>? onChange;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Expanded(
+          child: ColorPickerItem(
+            color: Color(0xff709CEE),
+          ),
+        ),
+        Expanded(
+          child: ColorPickerItem(
+            color: Color(0xff716EEC),
+          ),
+        ),
+        Expanded(
+          child: ColorPickerItem(
+            color: Color(0xffEA4F47),
+            isSelected: true,
+          ),
+        ),
+        Expanded(
+          child: ColorPickerItem(
+            color: Color(0xffE57656),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ColorPickerItem extends StatelessWidget {
+  const ColorPickerItem({
+    super.key,
+    required this.color,
+    this.isSelected = false,
+  });
+
+  final Color color;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: Container(
+        margin: isSelected ? EdgeInsets.zero : const EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+          border: isSelected
+              ? Border.all(
+                  color: Colors.black45,
+                  width: 4,
+                )
+              : null,
         ),
       ),
     );
