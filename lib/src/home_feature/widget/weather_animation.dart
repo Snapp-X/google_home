d13e779058555e03d417af8dd6ebfb18a0398554
Payam Zahedi <pzahedi1995@gmail.com>
2023-10-23 10:20:11 +0330

    add switch animation

--- lib/src/home_feature/widget/weather_animation.dart
+++ lib/src/home_feature/widget/weather_animation.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_home/src/home_feature/home_controller.dart';
+// import 'package:google_home/src/home_feature/home_controller.dart';
 import 'package:google_home/src/home_feature/home_state.dart';
 import 'package:rive/rive.dart';
+
+final typeProvider = StateProvider<WeatherType>((ref) {
+  return WeatherType.sunny;
+});
 
 final weatherRiveProvider = FutureProvider<RiveFile>((ref) async {
   final data = await rootBundle.load('assets/rive/weather.riv');
@@ -20,20 +24,33 @@ class WeatherAnimation extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final weatherFile = ref.watch(weatherRiveProvider);
 
-    final weather = ref.watch(
-      homeValuesProvider.select((value) => value.todayWeather),
-    );
+    // final weather = ref.watch(
+    //   homeValuesProvider.select((value) => value.todayWeather),
+    // );
+
+    final weatherType = ref.watch(typeProvider);
 
     return weatherFile.maybeWhen(
       data: (data) {
-        return RiveAnimation.direct(
-          data,
-          artboard: 'weather',
-          animations: [
-            weatherTypeToAnimationName(
-                weather?.weatherType ?? WeatherType.sunny),
-          ],
-          fit: BoxFit.cover,
+        return GestureDetector(
+          onTap: () {
+            final nextWeatherType = switch (weatherType) {
+              WeatherType.sunny => WeatherType.partiallySunny,
+              WeatherType.partiallySunny => WeatherType.cloudy,
+              WeatherType.cloudy => WeatherType.rainy,
+              WeatherType.rainy => WeatherType.sunny,
+            };
+
+            ref.read(typeProvider.notifier).state = nextWeatherType;
+          },
+          child: RiveAnimation.direct(
+            data,
+            artboard: 'weather',
+            animations: [
+              weatherTypeToAnimationName(weatherType),
+            ],
+            fit: BoxFit.cover,
+          ),
         );
       },
       orElse: () => const SizedBox.shrink(),
